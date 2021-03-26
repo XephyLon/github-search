6e5075364e7e1c146d3dacf6c401403a45df2ab1
Xephy Lon <adr3nalinexin@gmail.com>
2021-03-26 14:42:00 +0200

    :art: structure: Separated the application shell in a shared component

---
+++ src/app/shared/shell/shell.component.ts
@@ -0,0 +1,22 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { faAngular, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { SearchParamsModel } from "src/app/search/models/search-params.model";
+import { SearchService } from "src/app/search/search.service";
+
+@Component({
+  selector: 'app-shell',
+  templateUrl: './shell.component.html',
+  styleUrls: ['./shell.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ShellComponent {
+  faGithub = faGithub
+  faAngular = faAngular
+
+  constructor(private ss: SearchService) {}
+
+  getData(query: string) {
+    this.ss.query = query;
+    return this.ss.get();
+  }
+}
