b1e7422ac235c53202608255597f8ba285ce586c
Xephy Lon <adr3nalinexin@gmail.com>
2021-03-26 14:31:31 +0200

    :sparkles: routing: Added the search page route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'search',
+    pathMatch: 'prefix'
+  },
+  {
+    path: 'search',
+    loadChildren: () => import('./search/search.module').then(m => m.SearchModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
