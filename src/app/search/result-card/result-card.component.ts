3cb6ea41234ad5c8a3242dbd4e3544d7b88a1406
Xephy Lon <adr3nalinexin@gmail.com>
2021-03-27 10:17:39 +0200

    :recycle: refactor: ResultCard now displays more info

--- src/app/search/result-card/result-card.component.ts
+++ src/app/search/result-card/result-card.component.ts
@@ -5,11 +5,16 @@ import {
   Input,
   OnInit,
 } from '@angular/core';
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 import {
   faExternalLinkAlt,
+  faEye,
+  faGlobe,
   faIdCard,
+  faSignInAlt,
   faStar,
 } from '@fortawesome/free-solid-svg-icons';
+import { User } from "../models/search-response.model";
 @Component({
   selector: 'app-result-card',
   templateUrl: './result-card.component.html',
@@ -20,16 +25,16 @@ export class ResultCardComponent {
   faExternal = faExternalLinkAlt;
   faIdCard = faIdCard;
   faStar = faStar;
+  faGlobe = faGlobe;
+  faEye = faEye;
+  faSign = faSignInAlt;
+  faTwitter = faTwitter;
+
   
-  @Input() profilePicture!: string;
-  @Input() name!: string;
-  @Input() followerCount!: number;
-  @Input() starCount!: number;
-  @Input() description!: string;
-  @Input() profileUrl!: string;
+  @Input() dataSource!: User
   
   @HostListener('click') onClick() {
-    return window.open(this.profileUrl)
+    return window.open(this.dataSource.html_url)
   }
   constructor() {}
 }
